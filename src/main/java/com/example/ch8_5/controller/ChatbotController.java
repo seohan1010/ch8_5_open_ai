b79d9262095d380708beea19a60d090c3c6cc37f
seohan1010 <SEOhan12@>
2024-12-01 21:20:37 +0900

    ai dependency and code removed commit

--- src/main/java/com/example/ch8_5/controller/ChatbotController.java
+++ src/main/java/com/example/ch8_5/controller/ChatbotController.java
@@ -19,18 +19,6 @@ public class ChatbotController {
     Map<String, Object> map = null;
 
 
-    private final EmbeddingModel embeddingModel;
-
-    @Autowired
-    public ChatbotController(EmbeddingModel embeddingModel) {
-        this.embeddingModel = embeddingModel;
-    }
-
-    @GetMapping("/ai/embedding")
-    public Map embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        EmbeddingResponse embeddingResponse = this.embeddingModel.embedForResponse(List.of(message));
-        return Map.of("embedding", embeddingResponse);
-    }
 
 
 
